0d5d222e0c7b73431c1ca00ac6f25170670eaebc
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-01-26 16:02:49 +0900

    [level 0] Title: 더 크게 합치기, Time: 12.42 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(int a, int b) {
         int case1 = Integer.parseInt("" + a + b);
         int case2 = Integer.parseInt("" + b + a);
-        return case1 < case2 ? case2 : case1;
+        return Math.max(case1, case2);
     }
 }
