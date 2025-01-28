b12fca3609049f8d77f94b380e6bae83ceff0021
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-01-28 13:11:54 +0900

    [level 0] Title: 커피 심부름, Time: 0.05 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
@@ -2,23 +2,11 @@
 
 using namespace std;
 
-int solution(vector<string> order) {
-    std::unordered_map<std::string, int> prices;
-    prices.insert({"iceamericano", 4500});
-    prices.insert({"americanoice", 4500});
-    prices.insert({"hotamericano", 4500});
-    prices.insert({"americanohot", 4500});
-    prices.insert({"americano", 4500});
-    
-    prices.insert({"anything", 4500});
-    
-    prices.insert({"icecafelatte", 5000});
-    prices.insert({"cafelatteice", 5000});
-    prices.insert({"hotcafelatte", 5000});
-    prices.insert({"cafelattehot", 5000});
-    prices.insert({"cafelatte", 5000});
-    
+int solution(vector<string> order) {    
     int ans = 0;
-    for(const std::string &str : order) ans += prices[str];
+    for(const std::string &str : order) {
+        if(str.find("cafelatte") != std::string::npos) ans += 5000;
+        else ans += 4500;
+    }
     return ans;
 }
