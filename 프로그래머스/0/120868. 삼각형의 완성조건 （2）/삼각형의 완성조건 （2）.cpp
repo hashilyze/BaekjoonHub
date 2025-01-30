10e4b822b74310e5bf91247e995ce5546af21a92
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-01-30 14:34:03 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
@@ -3,22 +3,5 @@
 using namespace std;
 
 int solution(vector<int> sides) {
-    std::sort(sides.begin(), sides.end());
-    
-    int cnt = 0;
-    {
-        // b = sides[0]; c = sides[1];
-        // (a <= c < a + b) -> (c - b < a <= c)
-        int lower = sides[1] - sides[0];
-        int upper = sides[1];
-        cnt += upper - lower;
-    }
-    {
-        // a = sides[0]; b = sides[1];
-        // b <= c < a + b
-        int lower = sides[1];
-        int upper = sides[1] + sides[0];
-        cnt += upper - lower - 1;
-    }
-    return cnt;
+    return 2 * std::min(sides[0], sides[1]) - 1;
 }
