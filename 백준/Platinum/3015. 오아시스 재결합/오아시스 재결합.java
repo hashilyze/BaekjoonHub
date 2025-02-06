4508cc4d8aedf9cea279a946de15df5ee4dd6c12
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-07 00:08:37 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 328 ms, Memory: 63296 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -8,33 +8,28 @@ public class Main {
 	
 	
 	public static void main(String[] args) throws IOException {
-		long N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
 		long cnt = 0;
-		Deque<long[]> stk = new ArrayDeque<long[]>();
-		stk.offerLast(new long[] {2147483648L, 1});
+		Deque<int[]> stk = new ArrayDeque<int[]>();
 		for(int i = 0; i < N; ++i) {
-			long height = Integer.parseInt(br.readLine());
+			int height = Integer.parseInt(br.readLine());
 			// height를 push했을 때 단조증가수열을 유지하도록 변경
-			while(stk.peekLast()[0] < height) {
+			while(!stk.isEmpty() && stk.peekLast()[0] < height) {
 				cnt += stk.peekLast()[1];
 				stk.pollLast();
 			}
-			if(stk.size() > 1) { 
+			if(!stk.isEmpty()) {
 				if(stk.peekLast()[0] == height) {
 					cnt += stk.peekLast()[1];
-					if(stk.size() > 2) ++cnt;
-				} else {
-					++cnt;
+					if(stk.size() > 1) ++cnt;
+					++stk.peekLast()[1];
+					continue;
 				}
+				++cnt;
 			}
-			if(stk.peekLast()[0] == height) {
-				++stk.peekLast()[1];
-			} else {
-				stk.offerLast(new long[]{height, 1});
-			}
-			
+			stk.offerLast(new int[]{height, 1});
 		}
-		bw.append(cnt+"").flush();
+		bw.append(cnt+"").flush();;
 	}
 }
