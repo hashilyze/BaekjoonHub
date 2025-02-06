9b0591bca74709d9b13ffc40789f1970f728720f
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-06 17:11:25 +0900

    [Silver III] Title: 개미, Time: 68 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -6,15 +6,10 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st = null;
-	// Constants
-	static final int[] delta = { 1, 1 };
 	// Variables
 	static int w, h;
 	static int p, q;
 	static int t;
-	
-	static void reflectVertical() { delta[0] *= -1; }
-	static void reflectHorizontal() { delta[1] *= -1; }
 	
 	public static void main(String[] args) throws IOException {
 		// Inputs
@@ -34,30 +29,9 @@ public class Main {
 	}
 	
 	static void solution() {
-		if(p == w) reflectVertical();
-		if(q == h) reflectHorizontal();
-		
-		move();
-		int sp = p, sq = q, st = t;
-		int[] sdelta = delta.clone();
-		
-		while(t > 0) {
-			move();
-			
-			if(p == sp && q == sq && Arrays.equals(delta, sdelta)) {
-				t %= (st - t);
-			}
-		}
-	}
-	static void move() {
-		int min = t;
-		min = Math.min(min, delta[0] > 0 ? w - p : p);
-		min = Math.min(min, delta[1] > 0 ? h - q : q);
-		p += min * delta[0];
-		q += min * delta[1];
-		t -= min;
-		
-		if(p == 0 || p == w) reflectVertical();
-		if(q == 0 || q == h) reflectHorizontal();
+		p = (t + p) % (w << 1);
+		q = (t + q) % (h << 1);
+		if(w < p) p = (w << 1) - p;
+		if(h < q) q = (h << 1) - q;
 	}
 }
