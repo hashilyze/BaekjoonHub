db8f9c6117e571accd45e913fa11dc3614aa2eea
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-09 20:13:48 +0900

    [Gold V] Title: 괄호의 값, Time: 68 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -6,60 +6,27 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st = null;
 	
-	static final char OPEN2 = '(';
-	static final char CLOSE2 = ')';
-	static final char OPEN3 = '[';
-	static final char CLOSE3 = ']';
-	
 	public static void main(String[] args) throws IOException {
 		String txt = br.readLine();
 		int ans = solution(txt);
 		bw.append(""+ans).flush();
 	}
 	
-	static boolean isValid(String txt) {
-		Deque<Character> stk = new ArrayDeque<Character>();
-		for(char ch : txt.toCharArray()) {
-			if(ch == OPEN2 || ch == OPEN3) {
-				stk.offerLast(ch);
-			} else {
-				if(stk.isEmpty()) return false;
-				
-				if(stk.peekLast() == OPEN2) {
-					if(ch != CLOSE2) return false;
-				} else { // stk.peekLast() == OPEN3
-					if(ch != CLOSE3) return false;
-				}
-				stk.pollLast();
-			}
-		}
-		if(!stk.isEmpty()) return false;
-		return true;
-	}
-	
 	static int solution(String txt) {
-		if(!isValid(txt)) return 0;
-		
-		Deque<Integer> stk = new ArrayDeque<Integer>();
-		stk.offerLast(0);
-		
-		for(char ch : txt.toCharArray()) {
-			if(ch == OPEN2 || ch == OPEN3) {
-				stk.offerLast(-ch);
-			} else {
-				int val = ch == CLOSE2 ? 2 : 3;
-				if(stk.peekLast() > 0) { // (x)
-					val *= stk.pollLast();
-				}
-				
-				stk.pollLast();
-				if(stk.peekLast() > 0) { // x()
-					val += stk.pollLast();
-				}
-				
-				stk.offerLast(val);
+		int ans = 0;
+		int tmp = 1;
+		Deque<Character> stk = new ArrayDeque<Character>();
+		for(int i = 0; i < txt.length(); ++i) {
+			char ch = txt.charAt(i);
+			if(ch == '(' || ch == '[') {
+				tmp *= ch == '(' ? 2 : 3;
+				stk.offerLast(ch);
+			} else if(ch == ')' || ch == ']') {
+				if(stk.isEmpty() || Math.abs(stk.pollLast() - ch) > 2) return 0;
+				if(txt.charAt(i - 1) == '(' || txt.charAt(i - 1) == '[') ans += tmp;
+				tmp /= ch == ')' ? 2 : 3;
 			}
 		}
-		return stk.peekLast();
+		return stk.isEmpty() ? ans : 0;
 	}
 }
