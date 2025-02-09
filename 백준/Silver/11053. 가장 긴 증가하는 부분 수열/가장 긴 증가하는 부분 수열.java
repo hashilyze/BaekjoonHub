ce78733ca22cacc315c90acf179a85a99bdc2adf
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-09 23:58:47 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 76 ms, Memory: 12004 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -13,17 +13,14 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; ++i) arr[i] = Integer.parseInt(st.nextToken());
 		
-		List<Integer> li = new ArrayList<Integer>();
+		List<Integer> li = new ArrayList<Integer>(N);
 		li.add(arr[0]);
 		for(int i = 1; i < N; ++i) {
-			int pos = Collections.binarySearch(li, arr[i]);
-			if(pos < 0) {
-				pos = -pos - 1;
-			}
-			
-			if(pos == li.size()) {
+			if(li.get(li.size() - 1) < arr[i]) {
 				li.add(arr[i]);
 			} else {
+				int pos = Collections.binarySearch(li, arr[i]);
+				if(pos < 0) pos = -pos - 1;
 				li.set(pos, arr[i]);
 			}
 		}
