64b541825f89069b1f8de4f2fef33a14d9aa8ef7
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-12 23:44:45 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 72 ms, Memory: 11916 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,28 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	// Input Handler
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st = null;
-
-	public static void main(String[] args) throws IOException {
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; ++i) arr[i] = Integer.parseInt(st.nextToken());
-		
-		List<Integer> li = new ArrayList<Integer>(N);
-		li.add(arr[0]);
-		for(int i = 1; i < N; ++i) {
-			if(li.get(li.size() - 1) < arr[i]) {
-				li.add(arr[i]);
+	// constants
+	// variables
+	static int N;
+	static int[] seq = new int[1_000];
+	static int[] result = new int[1_000];
+	
+	// val과 같거나 큰 원소중 가장 왼쪽 원소의 인덱스를 반환
+	static int lower_bound(int val, int[] arr, int beg, int end) {
+		int left = beg, right = end - 1;
+		while(left < right) {
+			int mid = (left + right) / 2;
+			if(arr[mid] < val) {
+				left = mid + 1;
 			} else {
-				int pos = Collections.binarySearch(li, arr[i]);
-				if(pos < 0) pos = -pos - 1;
-				li.set(pos, arr[i]);
+				right = mid;
 			}
 		}
-		bw.append("" + li.size()).flush();
+		return left;
+	}
+	
+	static int solution() {
+		result[0] = seq[0];
+		int size = 1;
+		for(int i = 1; i < N; ++i) {
+			if(result[size - 1] < seq[i]) {
+				result[size++] = seq[i];
+			} else {
+				int at = lower_bound(seq[i], result, 0, size);
+				result[at] = seq[i];
+			}
+		}
+		return size;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i< N; ++i) seq[i] = Integer.parseInt(st.nextToken());
+		bw.append(""+solution()).flush();
 	}
 }
+	
