30662a24f545914166cac8f145b43589b644bfed
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-13 16:40:31 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 120 ms, Memory: 19408 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -15,6 +15,7 @@ public class Main {
 	// variables
 	static int N, M, D;
 	static int[][] mat = new int[MAX_SIZE][MAX_SIZE];
+	static int[][] field = new int[MAX_SIZE][MAX_SIZE];
 	static int endLine = MAX_SIZE; // 가장 뒤에 있는 적의 행 번호
 	static int numEnemy = 0;
 	static int result = 0;
@@ -53,7 +54,6 @@ public class Main {
 		int cnt = 0;
 		
 		// 초기화
-		int[][] field = new int[N][M];
 		for(int y = 0; y< N; ++y) {
 			for(int x = 0; x< M; ++x) {
 				field[y][x] = mat[y][x];
