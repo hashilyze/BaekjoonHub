977be52a2b44d8d9128a8089a36d6fcbf5cc254f
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-13 23:42:45 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 68 ms, Memory: 11908 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -12,14 +12,14 @@ public class Main {
 	static int N;
 	static int[] seq = new int[MAX_N];
 	static int[] buffer = new int[MAX_N];
-	static int[] dp = new int[MAX_N];
+	static int[] parents = new int[MAX_N];
 	static int[] lis= new int[MAX_N];
 	
 	
 	static int lower_bound(int val, int[] arr, int lo, int hi) {
 		while(lo < hi) {
 			int mid = (lo + hi) >> 1;
-			if(arr[mid] < val) {
+			if(seq[arr[mid]] < val) {
 				lo = mid + 1;
 			} else {
 				hi = mid;
@@ -31,37 +31,19 @@ public class Main {
 	static int getLIS(int beg, int end) {
 		int size = 0;
 		for(int i = beg; i < end; ++i) {
-			int at;
-			if(size == 0 || buffer[size - 1] < seq[i]) {
-				at = size++;
-			} else {
-				at = lower_bound(seq[i], buffer, 0, size - 1);
-			}
-			buffer[at] = seq[i];
-			dp[i] = at + 1;
+			int at = lower_bound(seq[i], buffer, 0, size);
+			if(at == size) ++size;
+			buffer[at] = i;
+			parents[i] = buffer[at != 0 ? at - 1 : i];
 		}
 		
-		// LIS 최대 길이일 때, 가장 오른쪽 원소의 인덱스를 구한다. 인덱스: ix, 길이: il, 값: iv
-		// ix - 1 이하의 인덱스에서 왼쪽으로 순회하며, 
-		// 길이가 (il - 1)이면서, 값이 ix보다 작은 인덱스를 구한다.
-		// <ix, il, iv>를 그것으로 갱신한다.
-		// il이 1이 될때까지 반복한다.
-		
-		int ix = beg;
-		for(int i = beg; i < end; ++i) {
-			if(dp[ix] < dp[i]) {
-				ix = i;
-			}
+		int cur = size - 1;
+		int node = buffer[size - 1];
+		while(cur >= 0) {
+			lis[cur] = seq[node];
+			--cur;
+			node = parents[node];
 		}
-		
-		lis[dp[ix] - 1] = seq[ix];
-		for(int i = ix - 1; i >= 0; --i) {
-			if(dp[i] == dp[ix] - 1 && seq[i] < seq[ix]) {
-				ix = i;
-				lis[dp[ix] - 1] = seq[ix];
-				if(dp[ix] == 1) break;
-			}
-		}	
 		return size;
 	}
 	
