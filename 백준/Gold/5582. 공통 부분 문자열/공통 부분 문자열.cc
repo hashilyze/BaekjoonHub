2350230a7f2b32d1a036c9539f1eb321e9cf8970
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-17 23:10:09 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 28 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 
 constexpr int MAX_N = 4001;
-int dp[MAX_N][MAX_N];
+int dp[2][MAX_N];
 
 int LCS(const std::string& s1, const std::string& s2){
     int max = 0;    
     for(int i = 1, len1 = s1.length(); i <= len1; ++i){
+        int pi = (i + 1) % 2, ci = i % 2;
         for(int j = 1, len2 = s2.length(); j <= len2; ++j){
             if (s1[i - 1] == s2[j - 1]){
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                max = std::max(max, dp[i][j]);
+                dp[ci][j] = dp[pi][j - 1] + 1;
+                max = std::max(max, dp[ci][j]);
+            } else{
+                dp[ci][j] = 0;
             }
         }
     }
