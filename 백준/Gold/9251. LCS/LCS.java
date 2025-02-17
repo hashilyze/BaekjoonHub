19fbca9ce227e17ea213d0d69d878445daf31f55
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-17 22:50:58 +0900

    [Gold V] Title: LCS, Time: 92 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -7,27 +7,35 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st = null;
+	// constants
+	static final int MAX_N = 100_001;
 	// variables
+	static int N;
 	static String A, B;
-	static int[][] dp = new int[1001][1001];
+	static int[][] dp;
 	
 	static int solution() {
 		for(int i = 1; i <= A.length(); ++i) {
+			int prev_i = (i + 1) % 2;
+			int mod_i = i % 2;
+			
 			for(int j = 1; j <= B.length(); ++j) {
-				dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-				dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + (A.charAt(i - 1) == B.charAt(j - 1) ? 1 : 0));
+				if(A.charAt(i - 1) == B.charAt(j - 1)) {
+					dp[mod_i][j] = dp[prev_i][j - 1] + 1;
+				} else {
+					dp[mod_i][j] = Math.max(dp[prev_i][j], dp[mod_i][j - 1]);
+				}
 			}
 		}
-		
-		return dp[A.length()][B.length()];
+		return dp[A.length() % 2][B.length()];
 	}
 	
 	public static void main(String[] args) throws IOException {
 		A = br.readLine();
 		B = br.readLine();
+		dp = new int[2][B.length() + 1]; // 행은 동시에 두개만 존재하면 됨
 		
-		int ans = solution();
-		bw.append(ans+"").flush();
+		bw.append(solution()+"").flush();
 		
 	}
 }
