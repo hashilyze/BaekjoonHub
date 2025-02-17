10908b57653fa45839d6d03724e7e3dc8d0ec9cb
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-17 16:40:37 +0900

    [Gold III] Title: 다리 만들기, Time: 128 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -62,42 +62,39 @@ public class Main {
 			}
 		}
 		// 2. 다리를 연결한다.
-		Deque<Node> qOrg = new ArrayDeque<>();
-		Deque<Node> qNext = new ArrayDeque<>();
+		Deque<Node> q = new ArrayDeque<>();
 		for(int y = 0; y < N; ++y) {
 			for(int x = 0; x < N; ++x) {
 				if(mat[y][x] > 0) {
-					qNext.add(new Node(y, x, 0, mat[y][x]));
+					q.add(new Node(y, x, 0, mat[y][x]));
 				}
 			}
 		}
 		Node[][] isVisited = new Node[N][N];
-		while(!qNext.isEmpty()) {
-			Deque<Node> tmp = qOrg; qOrg = qNext; qNext = tmp;
-			int minLnegth = Integer.MAX_VALUE;
+		int minLnegth = Integer.MAX_VALUE;
+		
+		while(!q.isEmpty()) {
+			Node u = q.pollFirst();
 			
-			while(!qOrg.isEmpty()) {
-				Node u = qOrg.pollFirst();
-				
-				if(isVisited[u.y][u.x] != null) {
-					Node v = isVisited[u.y][u.x]; 
-					if(v.id == u.id) continue;
+			if(isVisited[u.y][u.x] != null) {
+				Node v = isVisited[u.y][u.x]; 
+				if(v.id != u.id) {
 					minLnegth = Math.min(minLnegth, v.length + u.length - 1);
 				}
-				isVisited[u.y][u.x] = u;
-				
-				for(int i = 0; i < DELTA.length; ++i) {
-					int ny = u.y + DELTA[i][1];
-					int nx = u.x + DELTA[i][0];
-					if(isInOfRange(ny, nx)) {
-						qNext.add(new Node(ny, nx, u.length + 1, u.id));
-					}				
-				}
+				continue;
 			}
+			isVisited[u.y][u.x] = u;
 			
-			if(minLnegth < Integer.MAX_VALUE) return minLnegth;
+			for(int i = 0; i < DELTA.length; ++i) {
+				int ny = u.y + DELTA[i][1];
+				int nx = u.x + DELTA[i][0];
+				if(isInOfRange(ny, nx)) {
+					q.add(new Node(ny, nx, u.length + 1, u.id));
+				}				
+			}
 		}
-		return -1;
+			
+		return minLnegth;
 	}
 	
 	
@@ -116,5 +113,4 @@ public class Main {
 		bw.flush();
 			
 	}
-
 }
