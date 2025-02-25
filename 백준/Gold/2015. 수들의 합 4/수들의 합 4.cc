285033e6267a4eaae36251e79bcc8c5d5a20e61e
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-25 10:51:49 +0900

    [Gold IV] Title: 수들의 합 4, Time: 40 ms, Memory: 12316 KB -BaekjoonHub

---
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+// macros
+#define FASTIO std::ios::sync_with_stdio(0); std::cin.tie(0);
+//types
+using ll = long long;
+// constnts
+// variables
+int N, K;
+int A[200'000];
+std::unordered_map<ll, ll> map;
+
+
+int main() {
+	FASTIO;
+
+	std::cin >> N >> K;
+
+	ll cnt = 0;
+	++map[0];
+	for (int i = 1; i <= N; ++i) {
+		std::cin >> A[i];
+		A[i] += A[i - 1];
+
+		cnt += map[A[i] - K];
+		++map[A[i]];
+	}
+	std::cout << cnt;
+
+	return 0;
+}
