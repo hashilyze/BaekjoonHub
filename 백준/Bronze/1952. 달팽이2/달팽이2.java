2f53401028b6f43725780d403c9a2218728fe37f
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-02-26 11:15:15 +0900

    [Bronze I] Title: 달팽이2, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Bronze/1952. 달팽이2/달팽이2.java
+++ 백준/Bronze/1952. 달팽이2/달팽이2.java
@@ -6,38 +6,12 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 	
-	static final int[][] DELTA = new int[][] {
-		{1, 0},
-		{0, 1}, 
-		{-1, 0}, 
-		{0, -1}, 
-	};
 	
 	static int M, N;
 	
-	
 	static int solution() {
-		boolean[][] mat = new boolean[M][N];
-		
-		int next = 0;
-		int cnt = 0;
-		int x = 0, y = 0, d = 0;
-		while(++next < M * N) {
-			mat[y][x] = true;
-			
-			int nx = x + DELTA[d][0];
-			int ny = y + DELTA[d][1];
-			if((nx < 0 || N <= nx || ny < 0 || M <= ny) || mat[ny][nx]) {
-				d = (d + 1) % DELTA.length;
-				nx = x + DELTA[d][0];
-				ny = y + DELTA[d][1];
-				++cnt;
-			}
-			x = nx;
-			y = ny;
-		}
-		
-		return cnt;
+		if(M > N) return 2 * N - 1;
+		return 2 * M - 2;
 	}
 	
 	public static void main(String[] args) throws IOException{
