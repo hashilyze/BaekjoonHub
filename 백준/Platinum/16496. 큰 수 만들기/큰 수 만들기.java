2c0803362a731f3417437b23e20deeec75739cae
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-03-06 22:25:29 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 192 ms, Memory: 18496 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
@@ -16,7 +16,23 @@ public class Main {
 		numbers = br.readLine().split(" ");
 		
 		Arrays.sort(numbers, 0, N, (lhs, rhs)->{
-			return -(lhs + rhs).compareTo(rhs + lhs);
+			int lenL = lhs.length();
+			int lenR = rhs.length();
+			
+			int cur = 0;
+			while(cur < lenL + lenR) {
+				char lch = 0;
+				if(cur < lenL) lch = lhs.charAt(cur);
+				else lch = rhs.charAt(cur - lenL);
+				
+				char rch = 0;
+				if(cur < lenR) rch = rhs.charAt(cur);
+				else rch = lhs.charAt(cur - lenR);
+				
+				if(lch != rch) return rch - lch;
+				++cur;
+			}
+			return 0;
 		});
 		
 		if(numbers[0].equals("0") && numbers[N - 1].equals("0")) {
