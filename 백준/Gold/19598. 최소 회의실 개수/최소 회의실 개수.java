02407984fecdec7cc0645549c42dfec4953d7930
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-03-07 17:36:43 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 264 ms, Memory: 19848 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -21,21 +21,21 @@ public class Main {
 	static int MAX_N = 100_000;
 	// variables
 	static int N;
-	static Node[] meetings = new Node[MAX_N << 1];
+	static PriorityQueue<Node> pq = new PriorityQueue<>();
 	
 	
 	public static void main(String[] args) throws IOException {
 		N = readInt();
 		for(int i = 0; i < N; ++i) {
-			meetings[i] = new Node(1, readInt());
-			meetings[i + N] = new Node(-1, readInt());
+			pq.add(new Node(1, readInt()));
+			pq.add(new Node(-1, readInt()));
 		}
-		Arrays.sort(meetings, 0, N << 1);
 		
 		int max = 0, cnt = 0;
-		for(int i = 0; i < N << 1; ++i) { 
-			if(meetings[i].type > 0) ++cnt;
-			else max = Math.max(max, cnt--);
+		while(!pq.isEmpty()) {
+			Node node = pq.poll();
+			cnt += node.type;
+			max = Math.max(max, cnt);
 		}
 		System.out.print(max);
 	}
