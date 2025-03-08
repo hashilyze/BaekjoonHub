f01610aa1892e865462cd0be04778ea2caa924ab
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-03-08 12:53:00 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 68 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -4,51 +4,42 @@ import java.util.*;
 public class Main {
 	// Input Handler
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st = null;
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 	// constants
+	static final int MAX_N = 1_000;
 	// variables
 	static int N;
-	static int[] seq = new int[1_000];
+	static int[] A = new int[MAX_N];
+	static int[] buffer = new int[MAX_N]; // LIS를 생성하는 데 사용할 버퍼
 	
-	
-	static int lower_bound(int val, int[] arr, int lo, int hi) {
-		while(lo < hi) {
-			int mid = (lo + hi) >> 1;
-			if(arr[mid] < val) {
-				lo = mid + 1;
-			} else {
-				hi = mid;
-			}
-		}
-		return lo;
-	}
-	
-	static int getLISLength(int beg, int end) {
-		if(beg >= end) return 0;
-		
-		int size = 1;
-		int[] lis = new int[N]; lis[0] = seq[beg];
-		for(int i = beg + 1; i < end; ++i) {
-			if(lis[size - 1] < seq[i]) {
-				lis[size++] = seq[i];
-			} else {
-				int at = lower_bound(seq[i], lis, 0, size - 1);
-				lis[at] = seq[i];
-			}
-		}
-		return size;
-	}
 	
 	static int solution() {
-		return getLISLength(0, N);
+		int length = 0; // LIS의 길이
+		for(int i = 0; i < N; ++i) {
+			int elem = A[i];
+			int j = length; // A[i]를 마지막 원소로 하는 LIS의 길이를 구함
+			while(j > 0 && buffer[j - 1] >= elem) {
+				--j;
+			}
+			buffer[j] = elem;
+			length = Math.max(length, j + 1);
+		}
+		return length;
 	}
 	
 	public static void main(String[] args) throws IOException {
-		N = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i< N; ++i) seq[i] = Integer.parseInt(st.nextToken());
-		bw.append(""+solution()).flush();
+		N = readInt();
+		for(int i = 0; i < N; ++i) A[i] = readInt();
+		System.out.print(solution());
+	}
+	
+	// 부호없는 정수 읽기
+	static int readInt() throws IOException {
+		int c, n = 0;
+		while((c = System.in.read()) < 0x20);
+		n = c & 0x0F;
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		return n;
 	}
 }
-	
