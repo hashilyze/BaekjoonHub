c1cbbd869df4d140423249d571d000c98d253f4b
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-03-27 15:43:11 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 101 ms, Memory: 28,544 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -10,71 +10,71 @@ public class Solution {
 	static class Task implements Comparable<Task> {
 		int time; // 작업 발생 시각
 		int type; // 0: 계단을 나감; 1: 계단으로 이동
-		int who;
 
-		public Task(int time, int type, int who) {this.time = time;this.type = type;this.who=who;}
+		public Task(int time, int type) {this.time = time;this.type = type;}
 
 		@Override
-		public int compareTo(Task o) {
+		public int compareTo(Task o) { // 빠른 작업 순; 나가는 작업이 우선
 			if(this.time != o.time) return this.time - o.time; 
 			return this.type - o.type;
 		}
-
+		
 		@Override
-		public String toString() {
-			return "Task [time=" + time + ", type=" + type + ", who=" + who + "]";
+		public String toString() { // 디버깅용 
+			return "Task [time=" + time + ", type=" + type + "]";
 		}
 	}
 	// constants
 	static final int STAIR_SIZE = 2; 	// 계단의 수
 	static final int PEOPLE_SIZE = 10; 	// 사람의 최대 수 
-	static final int MAX_HOLE = 3; 		// 동시에 계단을 이용할 수 있는 사람의 최대 수 -> 가상의 작업단위 '통로'
+	static final int MAX_HOLE = 3; 		// 동시에 계단을 이용할 수 있는 사람의 최대 수 -> 가상의 자원 단위 '통로'
 	static final int EXIT_STAIR = 0;	// 통로를 이용하던 사람이 통로를 반환하는 작업
 	static final int ENTER_STAIR = 1;	// 계단에 도착한 사람이 계단에 통로를 요청하는 작업
 	// variables
 	static int N;
-	static int stairCnt = 0; 
-	static int[][] stairs = new int[STAIR_SIZE][2];
-	static int[] stairDistance = new int[2];
-	static int peopleCnt = 0;
-	static int[][] people = new int[PEOPLE_SIZE][2];
+	static int stairCnt = 0;  // 계단의 수 (2로 고정)
+	static int[][] stairs = new int[STAIR_SIZE][2];  // 계단의 좌표
+	static int[] stairDistance = new int[2]; // 계단을 내려가는데 걸리는 시간
+	static int peopleCnt = 0; // 사람의 수
+	static int[][] people = new int[PEOPLE_SIZE][2]; // 사람의 좌표
 	
 	
 	// 맨헤튼 거리
 	static int distance(int x1, int y1, int x2, int y2) { return Math.abs(x1 - x2) + Math.abs(y1 - y2);  }
 	
-	static int simulate(int bitMask, int active) {
+	static int simulate(int bitMask, int stairId) {
 		int t = 0;
 		PriorityQueue<Task> pq = new PriorityQueue<>(); // 작업이 발생하는 시각을 저장
-		Deque<Integer> waiting = new ArrayDeque<>(); // 대기하는 사람의 번호가 저장
+		int cntWaiting = 0;		// 대기 중인 사람의 수
 		int cntIdle = MAX_HOLE; // 사용되지 않고 있는 통로의 수
 		
-		for(int i = 0; i < peopleCnt; ++i) {
-			if(((bitMask >> i) & 1) == active) {
-				int dist = distance(stairs[active][0], stairs[active][1], people[i][0], people[i][1]);
-				pq.offer(new Task(dist, 1, i));
+		for(int i = 0; i < peopleCnt; ++i) { // 계단을 이용할 사람을 수집
+			if(((bitMask >> i) & 1) == stairId) {
+				int dist = distance(stairs[stairId][0], stairs[stairId][1], people[i][0], people[i][1]);
+				pq.offer(new Task(dist, 1));
 			}
 		}
-		while(!pq.isEmpty()) {
-			Task task = pq.poll();
+		while(!pq.isEmpty()) { // 모든 사람이 계단을 내려가면 작업이 끝남
+			Task task = pq.poll(); // 작업이 발생하는 시점으로 시간 경과
 			t = task.time;
 			
 			if(task.type == 0) { // Exit
-				if(waiting.isEmpty()) {
+				if(cntWaiting == 0) { // 대기 중인 사람이 없음
 					++cntIdle;
-				} else {
-					pq.offer(new Task(t + stairDistance[active], 0, waiting.pollFirst()));
+				} else { // 대기 중인 사람에게 통로를 전달
+					pq.offer(new Task(t + stairDistance[stairId], 0));
+					--cntWaiting;
 				}
 			} else { // (task.type == 1); Enter
-				if(cntIdle > 0) {
+				if(cntIdle > 0) { // 현재 사용되지 않는 통로를 획득; 
 					--cntIdle;
-					pq.offer(new Task(t + stairDistance[active] + 1, 0, task.who));
+					pq.offer(new Task(t + stairDistance[stairId] + 1, 0));
 				} else { // 대기
-					waiting.offerLast(task.who);
+					++cntWaiting;
 				}
 			}
 		}
-		return t;
+		return t;  
 	}
 	
 	static int solution() {
@@ -82,6 +82,8 @@ public class Solution {
 		// 사람들이 계단을 선택하는 모든 경우에 수에 대해 최소 시간 탐색
 		min = Math.min(min, simulate(0x00, 0));
 		for(int bitMask = 0x01, limit = (0x01 << peopleCnt) - 1; bitMask < limit; ++bitMask) {
+			// 모든 사람이 계단을 내려가는 시간
+			// (계단 1을 내려가려는 사람이 모두 내려간 시간)과 (계단 2를 내려가려는 사람이 모두 내려간 시간) 중 더 긴 시간 
 			min = Math.min(min, Math.max(simulate(bitMask, 0), simulate(bitMask, 1)));
 		}
 		min = Math.min(min, simulate((0x01 << peopleCnt) - 1, 1));
