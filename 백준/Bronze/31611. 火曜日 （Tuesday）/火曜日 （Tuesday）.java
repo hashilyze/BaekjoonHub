2b5cdf730d21993755415c7ac5eedef75680dcf5
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-02 23:04:48 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	// types
+	// constants
+	// variables
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println(readInt() % 7 == 2 ? 1 : 0); 
+	}
+	
+	static int readInt() throws IOException {
+		int c, n = 0;
+		while((c = System.in.read()) <= 0x20);
+		n = c & 0x0F;
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F); 
+		return n;
+	}
+}
