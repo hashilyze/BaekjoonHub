cb554994f17d79e24a091006ae0b35498b4c6d11
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-07 09:16:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 92 ms, Memory: 12036 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -2,45 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// Input Handler
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
 	// types
 	// constants
-	static final int MAX_N = 100;
-	static final int MAX_W = 100_000;
+	static int MAX_K = 100_000;
 	// variables
 	static int N, K;
-	static int[] W = new int[MAX_N];
-	static int[] V = new int[MAX_N]; 
-	static int[] dp = new int[MAX_W + 1];  
+	static int[] dp = new int[MAX_K + 1];
 	
-	static int solution() {
-		for(int i = 0; i < N; ++i) { // item[i]
-			for(int w = K; w >= W[i]; --w) {
-				dp[w] = Math.max(dp[w], dp[w - W[i]] + V[i]);
-			}
-		}
-		return dp[K];
-	}
-	
-	public static void main(String[] args) throws IOException {
-		N = readInt();
-		K = readInt();
-		for(int i = 0; i < N; ++i) {
-			W[i] = readInt();
-			V[i] = readInt();
-		}
-		System.out.print(solution());
-	}
-	
-	// 부호없는 정수 읽기
-	static int readInt() throws IOException {
-		int c, n = 0;
-		while((c = System.in.read()) <= 0x20);
-		n = c & 0x0F;
-		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
-		return n;
-	}
+    public static void main(String[] args) throws IOException {
+    	N = readInt();
+    	K = readInt();
+    	for(int i = 0; i < N; ++i) {
+    		int W = readInt(), V = readInt();
+    		for(int j = K; j >= W; --j) {
+    			dp[j] = Math.max(dp[j], dp[j - W] + V); // 물건 i를 선택한 경우와 그렇지 않은 경우 중 최댓값
+    		}
+    	}
+    	System.out.println(dp[K]);
+    }
+    
+    static int readInt() throws IOException {
+    	int c, n = 0;
+    	while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+    	if(c == '\r') System.in.read();
+    	return n;
+    }
 }
