36672bb3b40bfb2f9d9294ec9e3a7f1ff26e086f
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-07 16:56:47 +0900

    [Gold III] Title: 소문난 칠공주, Time: 92 ms, Memory: 44812 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -2,85 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	// IO Handler
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
-	
-	static final char S = 'S';
-	static final char Y = 'Y';
-	static final int SIZE = 5;
-	static final int PICK = 7;
-	static final int[][] DELTA = new int[][] { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-	
-	
-	static final int[][] arr = new int[SIZE * SIZE][2];
-	static char[][] mat = new char[SIZE][SIZE];
-	static List<Integer> comb = new ArrayList<Integer>();
+	// types
+	// constants
+	static int SIZE = 5;
+	static int AREA = SIZE*SIZE; 
+	static int[][] DELTA = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+	// variables
+	static char[] mat = new char[AREA];
 	static int cnt = 0;
+	static boolean[] isVisited = new boolean[0x01 << AREA];
 	
-	
-	static boolean isLinked() {
-		boolean[][] isVisited = new boolean[SIZE][SIZE];
-		for(int e : comb) {
-			int x = arr[e][0];
-			int y = arr[e][1];
-			isVisited[y][x] = true;
-		}
-		int cnt = 1;
-		Deque<int[]> q = new ArrayDeque<int[]>();
-		isVisited[arr[comb.get(0)][1]][arr[comb.get(0)][0]] = false;
-		q.add(arr[comb.get(0)]);
+	static void getCount(int cntY, int cntS, int bits) {
+		if(isVisited[bits]) return;
+		isVisited[bits] = true;
 		
-		while(!q.isEmpty()) {
-			int[] u = q.pollFirst();
-			
-			for(int i = 0; i < DELTA.length; ++i) {
-				int ny = u[1] + DELTA[i][1], nx = u[0] + DELTA[i][0]; 
-				if(0 <= nx && nx < SIZE && 0 <= ny && ny < SIZE && isVisited[ny][nx]) {
-					isVisited[ny][nx] = false;
-					q.offerLast(new int[] {nx, ny});
-					++cnt;
-				}
-			}
-		}
-		return cnt == PICK;
-	}
-	static boolean isOverwhelm() {
-		int cntS = 0, cntY = 0;
-		for(int e : comb) {
-			int x = arr[e][0];
-			int y = arr[e][1];
-			
-			if(mat[y][x] == S) ++cntS;
-			else if(mat[y][x] == Y) ++cntY;
-		}
-		return cntS > cntY;
-	}
-	
-	static void nextCombination(int idx, int left) {
-		if(left == 0) {
-			if(isOverwhelm() && isLinked()) {
-				++cnt;
-			}
+		if(cntY >= 4) return; // 임도연파가 과반
+		
+		if(cntY + cntS == 7) {
+			++cnt;
 			return;
 		}
 		
-		for(int i = idx; i < SIZE * SIZE - left + 1; ++i) {
-			comb.add(i);
-			nextCombination(i + 1, left - 1);
-			comb.remove(comb.size() - 1);
+		for(int i = 0; i < AREA; ++i) { 
+			if((bits & (0x01 << i)) == 0) continue;
+			
+			int y = i / SIZE, x = i % SIZE;
+			for(int d = 0; d < DELTA.length; ++d) {
+				int nx = x + DELTA[d][0];
+				int ny = y + DELTA[d][1];
+				if(!(0 <= ny && ny < SIZE && 0 <= nx && nx < SIZE)) continue;
+				
+				int j = ny*SIZE+nx;
+				if((bits & (0x01 << j)) != 0) continue;
+								
+				if(mat[j] == 'Y') getCount(cntY + 1, cntS, bits|(0x01 << j));
+				else getCount(cntY, cntS + 1, bits|(0x01 << j));
+			}
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
-		for(int i = 0; i < SIZE * SIZE; ++i) arr[i] = new int[] { i / SIZE, i % SIZE};
-		
-		for(int y = 0; y < SIZE; ++y) {
-			String line = br.readLine();
+		for(int i = 0, y = 0; y < SIZE; ++y) {
+			char[] rows = br.readLine().toCharArray();; 
 			for(int x = 0; x < SIZE; ++x) {
-				mat[y][x] = line.charAt(x);
+				mat[i++] = rows[x];
 			}
 		}
-		nextCombination(0, PICK);
+		for(int i = 0; i < AREA; ++i) {
+			if(mat[i] == 'Y') getCount(1, 0, 0x01 << i);
+			else getCount(0, 1, 0x01 << i);
+		}
 		System.out.println(cnt);
 	}
+    
+    static int readInt() throws IOException {
+    	int c, n = 0;
+    	while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+    	if(c == '\r') System.in.read();
+    	return n;
+    }
 }
