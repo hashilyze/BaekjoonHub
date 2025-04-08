9da9975f314196fb37ead4868a21780117e73409
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-08 10:06:05 +0900

    [Gold III] Title: 앱, Time: 88 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -10,32 +10,29 @@ public class Main {
 	static int N, M;
 	static int[] memories = new int[MAX_N];
 	static int[] costs = new int[MAX_N];
-	static int costSum;
-	static int[] dp = new int[MAX_N * MAX_C + 1]; // 비용 i에서 메모리의 최댓값
+	static int[] dp = new int[MAX_N * MAX_C + 1]; // dp[i]: 비용 i로 확보할 수 있는 최대 메모리
 	
-    public static void main(String[] args) throws IOException {
-    	N = readInt();
-    	M = readInt();
-    	for(int i = 0; i < N; ++i) memories[i] = readInt();
-    	for(int i = 0; i < N; ++i) costSum += costs[i] = readInt();
-    	
-    	for(int i = 0 ; i < N; ++i) {
-    		for(int c = costSum; c >= costs[i]; --c) {
-    			dp[c] = Math.max(dp[c], dp[c - costs[i]] + memories[i]);
-    		}
-    	}
-    	
-    	for(int c = 0; ; ++c) { // 메모리를 M이상 획득할 수 있는 최소 비용 탐색
-    		if(dp[c] >= M) {
-    			System.out.println(c);
-    			break;
-    		}
-    	}
+	
+	public static void main(String[] args) throws IOException {
+		N = readInt(); M = readInt();
+		for(int i = 0; i < N; ++i) memories[i] = readInt();
+		for(int i = 0; i < N; ++i) costs[i] = readInt();
+		
+		for(int i = 0; i < N; ++i) {
+			for(int c = MAX_N * MAX_C; c >= costs[i]; --c) {
+				dp[c] = Math.max(dp[c], dp[c - costs[i]] + memories[i]);
+			}
+		}
+		
+		int c = -1;
+		while(dp[++c] < M);
+		System.out.print(c);
     }
     
     static int readInt() throws IOException {
     	int c, n = 0;
     	while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+    	if(c == '\r') System.in.read();
     	return n;
     }
 }
