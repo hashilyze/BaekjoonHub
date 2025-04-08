ef3ec3d449d83834adb9e9740d5bd01a631f7982
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-08 16:37:05 +0900

    [Gold V] Title: 전깃줄, Time: 68 ms, Memory: 11436 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// types
+	// constants 
+	static final int SIZE = 500;
+	// variables
+	static int N;
+	static int[] A = new int[SIZE + 1];
+	static int len = 0;
+	static int[] buffer = new int[SIZE + 1];
+	
+	static int lowerBound(int lo, int hi, int key) {
+		while(lo < hi) {
+			int mid = (lo + hi) >> 1;
+			if(buffer[mid] < key) lo = mid + 1; 
+			else hi = mid;
+		}
+		return lo;
+	}
+	
+	static int solution() {
+		for(int i = 0; i <= SIZE; ++i) {
+			if(A[i] == 0) continue;
+			
+			int at = lowerBound(0, len, A[i]);
+			buffer[at] = A[i];
+			if(at == len) ++len;
+		}
+		return N - len;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		N = readInt();
+		for(int i = 0; i < N; ++i) {
+			A[readInt()] = readInt();
+		}
+		System.out.println(solution());
+    }
+    
+    static int readInt() throws IOException {
+    	int c, n = 0;
+    	while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+    	if(c == '\r') System.in.read();
+    	return n;
+    }
+}
