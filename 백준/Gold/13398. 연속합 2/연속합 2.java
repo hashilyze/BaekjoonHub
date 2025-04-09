ffbe8bb18f5829b9363d09ee415d4aa7e192080d
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-09 17:02:32 +0900

    [Gold V] Title: 연속합 2, Time: 100 ms, Memory: 15480 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -8,24 +8,21 @@ public class Main {
 	static int MAX_N = 100_000;
 	// variables
 	static int N;
-	static int[] A = new int[MAX_N + 1];
-	static int[][] dp = new int[MAX_N + 1][2];
+	static int[] A = new int[MAX_N];
+	static int[][] dp = new int[MAX_N][2];
+	
 	
 	public static void main(String[] args) throws IOException {
 		N = readInt();
-		for(int i = 1; i <= N; ++i) A[i] = readInt();
+		for(int i = 0; i < N; ++i) A[i] = readInt();
 		
-		int max = A[1];
-		dp[1][0] = dp[1][1] = A[1];
-		for(int i = 2; i <= N; ++i) {
-			dp[i][0] = Math.max(dp[i - 1][0] + A[i], A[i]); 
-			dp[i][1] = Math.max(dp[i - 1][0], A[i] + dp[i - 1][1]);
+		int max = A[0];
+		dp[0][0] = dp[0][1] = A[0];
+		for(int i = 1; i < N; ++i) {
+			dp[i][0] = Math.max(dp[i - 1][0] + A[i], A[i]);
+			dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + A[i]);
 			max = Math.max(max, Math.max(dp[i][0], dp[i][1]));
 		}
-//		for(int i = 1; i <= N; ++i) System.out.print(dp[i][0] + ", ");
-//		System.out.println();
-//		for(int i = 1; i <= N; ++i) System.out.print(dp[i][1] + ", ");
-//		System.out.println();
 		System.out.print(max);
     }
     
