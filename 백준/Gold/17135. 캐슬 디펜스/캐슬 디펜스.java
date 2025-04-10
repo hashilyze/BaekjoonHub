ef36c7e00274db8199cc989a249d3a6d8624929a
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-10 16:13:52 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 116 ms, Memory: 19064 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -4,23 +4,22 @@ import java.util.*;
 public class Main {
 	// Input Handler
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 	
 	// constants
 	static int MAX_SIZE = 15;
-	static int[][] DELTA = new int[][] {
-		{-1, 0}, {0, -1}, {1, 0}
-	};
+	static int[][] DELTA = { {-1, 0}, {0, -1}, {1, 0} };
 	// variables
 	static int N, M, D;
 	static int[][] mat = new int[MAX_SIZE][MAX_SIZE];
 	static int[][] field = new int[MAX_SIZE][MAX_SIZE];
 	static int endLine = MAX_SIZE; // 가장 뒤에 있는 적의 행 번호
 	static int numEnemy = 0;
-	static int result = 0;
+	static int maxKill = 0;
 	
-	static boolean isSafePos(int y, int x) { return 0 <= y && y < N && 0 <= x && x < M; }
+	
+	static boolean inRange(int y, int x) { return 0 <= y && y < N && 0 <= x && x < M; }
 	
 	// 가장 가까운 적 찾기
 	static int[] findEnemy(int ay, int ax, int[][] field, int visitedFlag) {
@@ -37,7 +36,7 @@ public class Main {
 			
 			for(int i = 0; i < DELTA.length; ++i) {
 				int nx = ux + DELTA[i][0], ny = uy + DELTA[i][1];
-				if(isSafePos(ny, nx)  // 좌표 확인
+				if(inRange(ny, nx)  // 좌표 확인
 					&& (Math.abs(nx - ax) + Math.abs(ny - ay) < D) // 거리 확인 
 					&& (field[ny][nx] != visitedFlag) // 탐색 여부 확인
 					) {
@@ -49,7 +48,7 @@ public class Main {
 		return null; 
 	}
 	
-	static int simulation(boolean[] positions) {
+	static int simulation(int locations) {
 		int visitedFlag = 2;
 		int cnt = 0;
 		
@@ -63,7 +62,7 @@ public class Main {
 		for(int y = N; y > endLine; --y) { // 가장 마지막 적과 같은 라인에 도달할 때까지 병사가 전진
 			afterKill.clear();
 			for(int x = 0; x < M; ++x) { 
-				if(positions[x]) {
+				if(((locations >> x) & 1) == 1) {
 					int[] pos = findEnemy(y - 1, x, field, visitedFlag);
 					if(pos != null) { // 적 제거
 						afterKill.add(pos);
@@ -83,31 +82,27 @@ public class Main {
 		return cnt;
 	}
 	
-	static void nextCombination(int left, int idx, int size, boolean[] isUsed) {
+	static void nextCombination(int idx, int left, int locations) {
 		if(left == 0) {
-			result = Math.max(result, simulation(isUsed));
+			maxKill = Math.max(maxKill, simulation(locations));
 			return;
 		}
 		
-		for(int i = idx; i < size - left + 1; ++i) {
-			if(isUsed[i]) continue;
+		for(int i = idx; i < M - left + 1; ++i) {
+			if(((locations >> i) & 1) == 1) continue;
 			
-			isUsed[i] = true;
-			nextCombination(left - 1, i + 1, size, isUsed);
-			isUsed[i] = false;
+			nextCombination(i + 1, left - 1, locations | (0x01 << i));
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		D = Integer.parseInt(st.nextToken());
+		N = readInt();
+		M = readInt();
+		D = readInt();
 		
 		for(int y = 0; y < N; ++y) {
-			st = new StringTokenizer(br.readLine());
 			for(int x = 0; x < M; ++x) {
-				mat[y][x] = st.nextToken().charAt(0) - '0';
+				mat[y][x] = readInt();
 				
 				if(mat[y][x] == 1) {
 					endLine = Math.min(endLine, y);
@@ -116,8 +111,14 @@ public class Main {
 			}
 		}
 		
-		boolean[] isUsed = new boolean[M];
-		nextCombination(3, 0, M, isUsed);
-		bw.append(""+result).flush();
+		nextCombination(0, 3, 0x00);
+		System.out.print(maxKill);
+	}
+	
+	static int readInt() throws IOException {
+		int c, n = 0;
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		if(c == '\r') System.in.read();
+		return n;
 	}
 }
