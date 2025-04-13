5a46a3b72caa1ad63114d4d434d48c858447101b
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-13 17:48:26 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 256 ms, Memory: 26044 KB -BaekjoonHub

--- 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
@@ -8,89 +8,55 @@ public class Main {
 	static StringTokenizer st;
 	// types
 	// constants
-	static final int SIZE = 100_010; 
 	// variables
 	static int N, M;
-	static long[] arr = new long[SIZE];
-	static long[] segment = new long[SIZE << 2];
-	static long[] lazy = new long[SIZE << 2];
+	static long[] fenwick = new long[100_001];
 	
 	
-	static void lazyPropagate(int rangeL, int rangeR, int index) {
-		if(lazy[index] == 0) return;
-		segment[index] += lazy[index] * (rangeR - rangeL + 1);
-		if(rangeL != rangeR) { 
-			lazy[index * 2 + 1] += lazy[index];
-			lazy[index * 2 + 2] += lazy[index];
-		} 
-		lazy[index] = 0;
-	}
-	
-	static long initSegment(int rangeL, int rangeR, int index) {
-		if(rangeL == rangeR) return segment[index] = arr[rangeL];
-		int mid = (rangeL + rangeR) >> 1;
-		long lhs = initSegment(rangeL, mid, index * 2 + 1);
-		long rhs = initSegment(mid + 1, rangeR, index * 2 + 2);
-		return segment[index] = lhs + rhs;
-	}
-	
-	static long querySegment(int queryL, int queryR, int rangeL, int rangeR, int index) {
-		lazyPropagate(rangeL, rangeR, index);
-		if(rangeR < queryL || queryR < rangeL) return 0;
-		if(queryL <= rangeL && rangeR <= queryR) return segment[index];
-		
-		int mid = (rangeL + rangeR) >> 1;
-		long lhs = querySegment(queryL, queryR, rangeL, mid, index * 2 + 1);
-		long rhs = querySegment(queryL, queryR, mid + 1, rangeR, index * 2 + 2);
-		return lhs + rhs;
-	}
-	
-	static long updateSegment(int updateL, int updateR, long diff, int rangeL, int rangeR, int index) {
-		lazyPropagate(rangeL, rangeR, index);
-		if(rangeR < updateL || updateR < rangeL) return segment[index];
-		if(updateL <= rangeL && rangeR <= updateR) {
-			lazy[index] += diff;
-			lazyPropagate(rangeL, rangeR, index);
-			return segment[index];
+	static long queryFenwick(int pos) {
+		long sum = 0;
+		while(pos > 0) {
+			sum += fenwick[pos];
+			pos -= pos & -pos;
 		}
-		
-		int mid = (rangeL + rangeR) >> 1;
-		long lhs = updateSegment(updateL, updateR, diff, rangeL, mid, index * 2 + 1);
-		long rhs = updateSegment(updateL, updateR, diff, mid + 1, rangeR, index * 2 + 2);
-		return segment[index] = lhs + rhs;
+		return sum;
 	}
-	
+	static void updateFenwick(int pos, int diff) {
+		while(pos <= N) {
+			fenwick[pos] += diff;
+			pos += pos & -pos;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
-		// Input
 		N = readInt();
-		for(int i = 0; i < N; ++i) arr[i] = readInt();
-		initSegment(0, N - 1, 0);
+		for(int i = 1; i <= N; ++i) {
+			int v = readInt();
+			updateFenwick(i, v);
+			updateFenwick(i+1, -v);
+		}
 		
 		M = readInt();
-		for(int i = 0; i < M; ++i) {
-			int cmd = readInt();
-			if(cmd == 1) {
-				int l = readInt(), r = readInt(), d = readInt();
-				updateSegment(l - 1, r - 1, d, 0, N - 1, 0);
+		while(M-- > 0) {
+			if(readInt() == 1) {
+				int s = readInt(), e = readInt(), v = readInt();
+				updateFenwick(s, v);
+				updateFenwick(e+1, -v);
 			} else {
-				int p = readInt();
-				sb.append(querySegment(p - 1, p - 1, 0, N - 1, 0)).append("\n");
+				sb.append(queryFenwick(readInt())).append("\n");
 			}
 		}
-		// Output
-		System.out.print(sb);
+		System.out.println(sb);
 	}
 	
 	static int readInt() throws IOException{
-		int c, n = 0, s = 1;
-		while((c = System.in.read()) <= 0x20);
-		if(c == '-') {
+		int c, n = System.in.read() & 0x0F, s = 1;
+		if(n == ('-' & 0x0F)) {
 			s = -1;
-			c = System.in.read();
+			n = 0;
 		}
-		n = c & 0x0F;
 		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		if(c == '\r') System.in.read();
 		return n * s;
 	}
 }
