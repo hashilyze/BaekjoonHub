2782e1ce3c78a373b00402e40f126fae34b8c861
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-24 22:37:41 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 22728 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;	
+	// constants
+	static final int SIZE = 200_100;
+	// variables
+	static int N, K;
+	static int[] isVisited = new int[SIZE + 1];
+
+	
+	static void solution() {
+		Arrays.fill(isVisited, -1);
+		Deque<Integer> q = new ArrayDeque<Integer>();
+		q.offerLast(N);
+		isVisited[N] = 0;
+		
+		while(!q.isEmpty()) {
+			int x = q.pollFirst();
+			if(x == K) return;
+			
+			for(int nx : new int[] {x-1, x+1, x<<1}) {
+				if(nx < 0 || SIZE < nx) continue;
+		         if(isVisited[nx] != -1) continue;
+
+		         if(nx == x * 2) {
+		            q.offerFirst(nx);
+		            isVisited[nx] = isVisited[x];
+		         } else{
+		        	 q.offerLast(nx);
+		            isVisited[nx] = isVisited[x] + 1;
+		         }
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		N = readInt(); K = readInt();
+		solution();
+		System.out.print(isVisited[K]);
+	}
+	
+	static int readInt() throws IOException{
+		int c, n = 0;
+		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		if(c == '\r') System.in.read();
+		return n;
+	}
+}
