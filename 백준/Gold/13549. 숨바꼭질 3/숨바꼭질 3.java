251f6910706f400d204cbd12d20322b759b223b4
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-24 22:39:27 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 22680 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -5,50 +5,50 @@ public class Main {
 	// Input Handler
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;	
+	static StringTokenizer st;
 	// constants
 	static final int SIZE = 200_100;
 	// variables
 	static int N, K;
 	static int[] isVisited = new int[SIZE + 1];
 
-	
 	static void solution() {
 		Arrays.fill(isVisited, -1);
 		Deque<Integer> q = new ArrayDeque<Integer>();
 		q.offerLast(N);
 		isVisited[N] = 0;
-		
-		while(!q.isEmpty()) {
-			int x = q.pollFirst();
-			if(x == K) return;
-			
-			for(int nx : new int[] {x-1, x+1, x<<1}) {
-				if(nx < 0 || SIZE < nx) continue;
-		         if(isVisited[nx] != -1) continue;
 
-		         if(nx == x * 2) {
-		            q.offerFirst(nx);
-		            isVisited[nx] = isVisited[x];
-		         } else{
-		        	 q.offerLast(nx);
-		            isVisited[nx] = isVisited[x] + 1;
-		         }
+		while (!q.isEmpty()) {
+			int x = q.pollFirst();
+			if (x == K) return;
+
+			for (int nx : new int[] { x - 1, x + 1, x << 1 }) {
+				if (0 <= nx && nx <= SIZE && isVisited[nx] == -1) {
+					if (nx == x * 2) {
+						q.offerFirst(nx);
+						isVisited[nx] = isVisited[x];
+					} else {
+						q.offerLast(nx);
+						isVisited[nx] = isVisited[x] + 1;
+					}
+				}
 			}
 		}
 	}
-	
-	
+
 	public static void main(String[] args) throws IOException {
-		N = readInt(); K = readInt();
+		N = readInt();
+		K = readInt();
 		solution();
 		System.out.print(isVisited[K]);
 	}
-	
-	static int readInt() throws IOException{
+
+	static int readInt() throws IOException {
 		int c, n = 0;
-		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
-		if(c == '\r') System.in.read();
+		while ((c = System.in.read()) >= 0x30)
+			n = (n << 3) + (n << 1) + (c & 0x0F);
+		if (c == '\r')
+			System.in.read();
 		return n;
 	}
 }
