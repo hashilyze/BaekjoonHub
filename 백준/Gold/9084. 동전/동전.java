e2a8fffc7f7c1125c88045ddb326d9bcd5bcbee2
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-04-24 22:13:43 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 11436 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -2,10 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// Input Handler
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	// IO Handler
 	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
 	// types
 	// constants
 	// variables
@@ -13,33 +11,33 @@ public class Main {
 	static int[] coins = new int[20];
 	static int[] dp = new int[10_001];
 	
+	
 	static int solution() {
-		Arrays.fill(dp, 0, M + 1, 0);
+		Arrays.fill(dp, 1, M + 1, 0);
 		for(int i = 0; i < N; ++i) {
-			int coin = coins[i];
-			dp[coin] += 1;
-			for(int j = coin+1; j <= M; ++j) {
-				dp[j] += dp[j - coin];
+			for(int j = coins[i]; j <= M; ++j) {
+				dp[j] += dp[j - coins[i]];
 			}
 		}
-		return dp[M];
+		return dp[M]; 
 	}
 	
 	public static void main(String[] args) throws IOException {
-		int T = readInt();
-		while(T-- > 0) {
-			N = readInt();
-			for(int i = 0; i < N; ++i) coins[i] = readInt();
-			M = readInt();
-			sb.append(solution()).append('\n');
-		}
-		System.out.println(sb);
-	}
-	
-	static int readInt() throws IOException {
-		int c, n = 0;
-		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
-		if(c == '\r') System.in.read();
-		return n;
-	}
+		dp[0] = 1;
+    	int T = readInt();
+    	while(T-- > 0) {
+    		N = readInt();
+    		for(int i = 0; i < N; ++i) coins[i] = readInt();
+    		M = readInt();
+    		sb.append(solution()).append("\n");
+    	}
+    	System.out.println(sb);
+    }
+    
+    static int readInt() throws IOException {
+    	int c, n = 0;
+    	while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+    	if(c == '\r') System.in.read();
+    	return n;
+    }
 }
