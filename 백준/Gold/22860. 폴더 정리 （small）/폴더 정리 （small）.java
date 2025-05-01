1d23554778289c7423205f6c02a1fdf7bcc1f9d8
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-05-01 19:39:57 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 544 ms, Memory: 88464 KB -BaekjoonHub

---
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st = null;
+	// types
+	static class Node{
+		List<Node> subDict = new ArrayList<Node>(); // 바로 아래에 위치한 폴더
+		Set<String> fileNames = new HashSet<>(); // 현재 폴더 이하의 경로에 위치한 파일의 이름들
+		int fileNum; // 현재 폴더 이하의 경로에 위치한 파일의 수
+	}
+	// constants
+	// variables
+	static int N, M, Q;
+	static Node root = new Node();
+	// 디렉토리 이름에는 중복이 없다. -> 디렉토리 이름과 인덱스는 1:1 대응관게가 성립
+	static Map<String, Node> mapping = new HashMap<String, Node>();
+	
+	
+	static Node getNode(String name) {
+		Node node = mapping.getOrDefault(name, null); 
+		if(node == null) mapping.put(name, node = new Node());
+		return node;
+	}
+	
+	static void insert(String P, String F, int C) {
+		Node nodeP = getNode(P);
+		if(C == 0) { // F가 파일
+			++nodeP.fileNum;
+			nodeP.fileNames.add(F);
+		} else { // F가 폴더
+			Node nodeF = getNode(F);
+			nodeP.subDict.add(nodeF);
+		}
+	}
+	
+	static void build(Node root) {
+		for(Node next : root.subDict) {
+			build(next);
+			root.fileNum += next.fileNum;
+			root.fileNames.addAll(next.fileNames);
+		}
+	}
+	
+	static int[] query(String[] path) {
+		String target = path[path.length - 1];
+		Node node = mapping.get(target);
+		return new int[] {node.fileNames.size(),   node.fileNum};
+	}
+	
+	public static void main(String[] args) throws IOException {
+		root = new Node();
+		mapping.put("main", root);
+		
+		// 구성
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < N + M; ++i) {
+			st = new StringTokenizer(br.readLine());
+			String P = st.nextToken();
+			String F = st.nextToken();
+			int C = Integer.parseInt(st.nextToken());
+			
+			insert(P, F, C);
+		}
+		build(root);
+		// 쿼리
+		Q = Integer.parseInt(br.readLine());
+		while(Q-- > 0) {
+			String[] path = br.readLine().split("/");
+			int[] ans = query(path); 
+			sb.append(ans[0]).append(" ").append(ans[1]).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
