072a1546a68f32128537639d236b444fc6c4a7bc
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-05-01 15:54:37 +0900

    [Gold IV] Title: 전화번호 목록, Time: 292 ms, Memory: 39392 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -9,7 +9,7 @@ public class Main {
 	// constants
 	static final int SIZE = 10_000;	// 단어의 수
 	static final int LENGTH = 10;	// 단어의 최대 길이
-	static final int RANGE = 10;	// 문자의 수
+	static final int RANGE = 10;	// 문자(알파벳)의 수
 	static final int ROOT = 0;
 	static final int[] DEFAULT_NEXTS = new int[RANGE];
 	// types
@@ -17,6 +17,7 @@ public class Main {
 	static int size = 0;
 	static int[][] nexts = new int[SIZE * LENGTH + 1][RANGE];
 	static boolean[] isTerminals = new boolean[SIZE * LENGTH + 1];
+	// (트라이의 최대 노드 수) = (단어의 수 * 단어 당 최대 문자의 수)
 	
 	static boolean insert(char[] word) {
 		int cur = ROOT;
@@ -39,8 +40,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int T = Integer.parseInt(br.readLine());
 		while(T-- > 0) {
-			// 초기화
-			for(int i = 0; i <= size; ++i) System.arraycopy(DEFAULT_NEXTS, 0, nexts[i], 0, RANGE); 
+			// 초기화: 이전에 사용했던 자원 초기화
+			for(int i = 0; i <= size; ++i) {
+				// Arrays.fill(nexts[i], 0); 보다 빠르다.
+				System.arraycopy(DEFAULT_NEXTS, 0, nexts[i], 0, RANGE);
+			}
 			Arrays.fill(isTerminals, 0, size+1, false);
 			size = 0;
 			
