a1a1af15cca4a729b3f699d20fe0099938fdf59c
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-05-05 15:54:10 +0900

    [Gold V] Title: Not Equal, Time: 344 ms, Memory: 28308 KB -BaekjoonHub

---
+++ 백준/Gold/14926. Not Equal/Not Equal.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// Input Handler
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st = null;
+	// types
+	// constants
+	// variables
+	static int N;
+	static boolean[][] isVisited; 
+	static List<Integer> euler = new ArrayList<>();
+	
+	static void eulerPath(int u) {
+		for(int v = 0; v < N; ++v) {
+			if(isVisited[u][v]) continue;
+			isVisited[u][v] = isVisited[v][u] = true;
+			eulerPath(v);
+		}
+		euler.add(u);
+	} 
+	
+	public static void main(String[] args) throws IOException {
+		N = readInt();
+		isVisited = new boolean[N][N];
+		for(int u = 0; u < N; ++u) isVisited[u][u] = true;
+		
+		eulerPath(0);
+		for(int i = euler.size() - 1; i >= 0; --i) {
+			sb.append("a").append(euler.get(i)+1).append(" ");
+		}
+		System.out.print(sb);
+	}
+
+	static int readInt() throws IOException {
+		int c, n = 0;
+		while ((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
+		if (c == '\r') System.in.read();
+		return n;
+	}
+}
