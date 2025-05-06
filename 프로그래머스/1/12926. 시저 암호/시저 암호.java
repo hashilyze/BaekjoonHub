5ffd2099949baf485d7827c7a762d225b8a2a003
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-05-06 18:02:04 +0900

    [level 1] Title: 시저 암호, Time: 1.86 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s, int n) {
+        char[] chars = s.toCharArray();
+        for(int i = 0; i < chars.length; ++i) {
+            if(Character.isUpperCase(chars[i])){
+                chars[i] = (char)((chars[i] - 'A' + n) % 26 + 'A');
+            } else if(Character.isLowerCase(chars[i])){
+                chars[i] = (char)((chars[i] - 'a' + n) % 26 + 'a');
+            }
+        }
+        return new StringBuilder().append(chars).toString();
+    }
+}
