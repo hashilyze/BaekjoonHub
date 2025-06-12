08b20d20af7a3ac5e2a24317a955c6bf2c275767
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-06-12 12:40:10 +0900

    [Gold V] Title: 최소비용 구하기, Time: 184 ms, Memory: 20660 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -5,74 +5,82 @@ public class Main {
 	// Input Handler
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	
-	static class Node implements Comparable<Node>{
+
+	static class Node implements Comparable<Node> {
 		int vertex, weight;
-		
+
 		Node() { }
 		Node(int vertex, int weight) {
 			this.vertex = vertex;
 			this.weight = weight;
 		}
+
 		@Override
 		public int compareTo(Node other) {
 			return this.weight - other.weight;
 		}
 	}
-	
+
 	static final int MAX_N = 1_000;
 	static final int MAX_W = 100_000;
 	static final int INF = MAX_N * MAX_W;
-	
+
 	static int N, M, S, T;
 	static List<Node>[] adj = new List[MAX_N];
 	static int[] minDist = new int[MAX_N];
-	
-	
+
 	static void solution() {
 		Arrays.fill(minDist, 0, N, INF);
-		
+
 		PriorityQueue<Node> pq = new PriorityQueue<Node>();
 		pq.offer(new Node(S, 0));
 		minDist[S] = 0;
-		
-		while(!pq.isEmpty()) {
+
+		while (!pq.isEmpty()) {
 			Node node = pq.poll();
 			int u = node.vertex, w = node.weight;
-			if(u == T) break;
-			
-			for(Node next : adj[u]) {
+			if (u == T)
+				break;
+
+			for (Node next : adj[u]) {
 				int v = next.vertex, edge = next.weight;
-				if(w + edge < minDist[v]) {
+				if (w + edge < minDist[v]) {
 					minDist[v] = w + edge;
 					pq.offer(new Node(v, minDist[v]));
 				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		// Input
-		N = Integer.parseInt(br.readLine());
-		for(int i = 0; i < N; ++i) adj[i] = new ArrayList<Node>();
-		
-		M = Integer.parseInt(br.readLine());
-		for(int i = 0; i < M; ++i) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken()) - 1;
-			int v = Integer.parseInt(st.nextToken()) - 1;
-			int w = Integer.parseInt(st.nextToken());
+		N = readInt();
+		for (int i = 0; i < N; ++i)
+			adj[i] = new ArrayList<Node>();
+
+		M = readInt();
+		for (int i = 0; i < M; ++i) {
+			int u = readInt() - 1;
+			int v = readInt() - 1;
+			int w = readInt();
 			adj[u].add(new Node(v, w));
 		}
-		
-		st = new StringTokenizer(br.readLine());
-		S = Integer.parseInt(st.nextToken()) - 1;
-		T = Integer.parseInt(st.nextToken()) - 1;
-		
+
+		S = readInt() - 1;
+		T = readInt() - 1;
+
 		solution();
-		
+
 		// Output
 		System.out.print(minDist[T]);
+	}
+
+	static int readInt() throws IOException {
+		int c, n = 0;
+		while ((c = System.in.read()) >= 0x30)
+			n = (n << 3) + (n << 1) + (c & 0x0F);
+		if (c == '\r')
+			System.in.read();
+		return n;
 	}
 }
