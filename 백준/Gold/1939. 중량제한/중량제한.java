e44c846c5445ef6206b2f0ab3a54dff6c5b1baa6
Junsu Bae <43923425+hashilyze@users.noreply.github.com>
2025-07-10 16:55:48 +0900

    [Gold III] Title: 중량제한, Time: 228 ms, Memory: 24644 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,68 +1,78 @@
 import java.io.*;
 import java.util.*;
 
-import javax.print.attribute.HashAttributeSet;
-
 public class Main {
-	// Input Handler
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st = null;
-	// types
-	static class Node implements Comparable<Node>{
-		int v, w;
-		
-		Node(int v, int w) {this.v=v;this.w=w;}
+    // IO
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    // types
+    static class Node implements Comparable<Node> {
+        int to, transfer;
 
-		@Override
-		public int compareTo(Node o) { return o.w - this.w; } // 중량제한이 큰 간선 순으로 정렬
-	}
-	// constants
-	static final int MAX_N = 10_000;
-	// variables
-	static int N, M;
-	static int S, T;
-	static List<Node>[] adj = new List[MAX_N + 1];
-	static int[] maxFlow = new int[MAX_N + 1]; 
-	
-	static int solution() {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		maxFlow[S] = Integer.MAX_VALUE;
-		pq.offer(new Node(S, maxFlow[S]));
-		
-		while(!pq.isEmpty()) {
-			Node node = pq.poll();
-			if(node.v == T) return node.w;
-			
-			for(Node next : adj[node.v]) {
-				int flow = Math.min(node.w, next.w);
-				if(maxFlow[next.v] < flow) {
-					maxFlow[next.v] = flow;
-					pq.offer(new Node(next.v, flow));
-				}
-			}
-		}
-		return -1;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		N = readInt();
-		M = readInt();
-		for(int i = 1; i <= N; ++i) adj[i] = new ArrayList<>();
-		for(int i = 0; i < M; ++i) {
-			int a = readInt(), b = readInt(), c = readInt();
-			adj[a].add(new Node(b, c));
-			adj[b].add(new Node(a, c));
-		}
-		S = readInt(); T = readInt();
-		System.out.println(solution());
-	}
-	
-	static int readInt() throws IOException {
-		int c, n = 0;
-		while((c = System.in.read()) <= 0x20);
-		n = c & 0x0F;
-		while((c = System.in.read()) >= 0x30) n = (n << 3) + (n << 1) + (c & 0x0F);
-		return n;
-	}
+        Node(int to, int transfer){
+            this.to = to;
+            this.transfer = transfer;
+        }
+
+        @Override
+        public int compareTo(Node other) { return other.transfer - this.transfer; }
+    }
+    // variables
+    static int N, M;
+    static List<Node>[] adj;
+    static int S, E;
+    static int[] maxTransfers;
+
+    static int solution(){
+        PriorityQueue<Node> q = new PriorityQueue<>();
+
+        maxTransfers[S] = 0;
+        for(Node node : adj[S]) {
+            maxTransfers[node.to] = node.transfer;
+            q.offer(node);
+        }
+
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            if(node.to == E) break;
+
+            for(Node next : adj[node.to]){
+                // 전체 경로에서 최소 중량 만큼만 전달가능하다.
+                int nextTransfer = Math.min(next.transfer, node.transfer);
+                if(maxTransfers[next.to] < nextTransfer){
+                    maxTransfers[next.to] = nextTransfer;
+                    q.offer(new Node(next.to, nextTransfer));
+                }
+            }
+        }
+        return maxTransfers[E];
+    }
+
+    public static void main(String[] args) throws IOException {
+        N = readInt();
+        M = readInt();
+
+        adj = new ArrayList[N + 1];
+        maxTransfers = new int[N + 1];
+        for(int i = 1; i <= N; ++i) adj[i] = new ArrayList<>();
+
+        for(int i = 0; i < M; ++i) {
+            int s = readInt(), e = readInt(), w = readInt();
+            adj[s].add(new Node(e, w));
+            adj[e].add(new Node(s, w));
+        }
+
+        S = readInt();
+        E = readInt();
+
+        System.out.print(solution());
+    }
+
+    static int readInt() throws IOException{
+        int c, n = 0;
+        while((c = System.in.read()) > 0x20) n = (n << 3) + (n << 1) + (c & 0x0F);
+        if(c == '\r') System.in.read();
+        return n;
+    }
 }
